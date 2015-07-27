a62301d99f35413b8672cb1c119137addcea576e
Peter Parisi <peterparisi@Peters-MacBook-Pro.local>
2015-07-26 21:32:04 -0700

    Rest of files v1

---
+++ js/resumeBuilder.js
@@ -0,0 +1,53 @@
+var bio = {
+	"name": "Peter Parisi",
+	"role": "Inbound Sales Development Manager",
+	"contacts": {
+		"mobile": "\(503\) 866-1748",
+		"work": "\(415\) 680-1766",
+		"email": "pparisi@zenefits.com",
+		"github": "pparisi13",
+		"location": "San Francisco, CA",
+	},
+	"welcomeMessage": "I am passionate about helping people find solutions to real problems.",
+	"skills": [
+		"Sales", "Management", "Cold Calling"
+		],
+	"bioPic": "images/bioPic.jpg"
+}
+
+var education = {
+	"schools": [{
+		"name": "The College of Wooster",
+		"city": "Wooster, OH",
+		"degree": "Bachelor of Arts, Philosophy",
+		"majors": "Philosophy with a focus on Kantian Morality",
+		"dates": "2009 - 2013"
+		}
+	]
+}
+
+var work = {
+	"jobs": [{
+		"employer": "Zenefits",
+		"title": "Inbound Sales Development Manager",
+		"dates": "June 2015 - Present",
+		"description": "Zenefits helps businesses manage their HR and benefits for free."
+	},{
+		"employer": "Zenefits",
+		"title": "Outbound Sales Development Manager",
+		"dates": "April 2015 - June 2015"
+	},{
+		"employer": "Zenefits",
+		"title": "Sales Development Representative",
+		"dates": "September 2014 - April 2015",
+		"description": "Achieved 121% of quota over 9 months, most referring revenue, etc. etc."
+	}]
+}
+
+var projects = {
+	"projects": [{
+		"title": "Resume building through Udacity.",
+		"dates": "July - Present",
+		"description": "Everything you see here is a result of my intro to JavaScript course."
+	}]
+}
